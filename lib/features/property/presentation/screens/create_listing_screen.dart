f1b6c455a1966879aefa32e3589e9f11c70fc40c
Misganaw Habtamu <misganawa956@gmail.com>
2026-05-03 23:29:15 +0300

    create_listing_screen

---
+++ lib/features/property/presentation/screens/create_listing_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CreateListingScreen extends StatelessWidget {
+  const CreateListingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Create Listing Screen')),
+    );
+  }
+}
