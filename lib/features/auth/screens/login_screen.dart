a954be23096af9f7fef3bb93dc6586d35be9b234
effileo <fitaalemayehu12@gmail.com>
2026-05-05 09:09:45 +0300

    deleting unused imports

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:go_router/go_router.dart';
 import 'package:bet/core/constants/app_colors.dart';
-import 'package:bet/core/widgets/app_logo.dart';
 import 'package:bet/core/widgets/custom_app_bar.dart';
 import 'package:bet/core/widgets/custom_button.dart';
 import 'package:bet/core/widgets/custom_text_field.dart';
