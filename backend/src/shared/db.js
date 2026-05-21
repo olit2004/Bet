c6e0306705f6315edeeaa247bc7b68254ca4ad28
effileo <fitaalemayehu12@gmail.com>
2026-05-21 21:48:18 +0300

    fix: update db connection and format schema

--- backend/src/shared/db.js
+++ backend/src/shared/db.js
@@ -1,7 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    },
+  },
   log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['error'],
 });
 
 module.exports = prisma;
+
+// triggered restart
