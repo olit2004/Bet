8c5fc8e5ca44b2272939d40923f7f92fff9f912f
Misganaw Habtamu <misganawa956@gmail.com>
2026-05-22 17:40:55 +0300

    seller_profile entity

---
+++ lib/features/seller/domain/entities/seller_profile_stats.dart
@@ -0,0 +1,13 @@
+class SellerProfileStats {
+  final int activeProperties;
+  final int totalBids;
+  final int totalViews;
+  final String conversionRate;
+
+  SellerProfileStats({
+    required this.activeProperties,
+    required this.totalBids,
+    required this.totalViews,
+    required this.conversionRate,
+  });
+}
